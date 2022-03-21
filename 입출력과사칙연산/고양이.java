d684c429bcb27a71814908e249713302719089a8
이정윤 <dlwjddbs14@naver.com>
2022-03-22 08:44:36 +0900

    🥕 :: 10171 : 고양이

---
+++ 입출력과사칙연산/고양이.java
@@ -0,0 +1,8 @@
+public class 고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
