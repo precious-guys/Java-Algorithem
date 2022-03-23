326186cb6d3fd6bb66ad5303a33e5e0d1258c3ce
이정윤 <dlwjddbs14@naver.com>
2022-03-23 21:18:21 +0900

    🥕 :: 18108 : 1998년생인 내가 태국에서는 2541년생?!

---
+++ 입출력과사칙연산/1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 1998년생인내가태국에서는 2541년생?! {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println(a - 543);
+    }
+}
