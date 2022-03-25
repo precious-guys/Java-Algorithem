83049b3637c4645c59feab2e90c7688a12f5f42b
이정윤 <dlwjddbs14@naver.com>
2022-03-25 23:51:21 +0900

    🥕 :: 3052 : 나머지

---
+++ 일차원배열/나머지.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class 나머지 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> h = new HashSet<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            h.add(sc.nextInt() % 42);
+        }
+        sc.close();
+        System.out.println(h.size());
+    }
+}
